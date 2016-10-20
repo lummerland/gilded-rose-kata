87902aabeed88981bc050b81f5b4655983d74162
Manuel Kummerländer <mkummerlaender@seibert-media.net>
2016-10-20 11:24:33 +0200

    first approach to make the project testable

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -1,36 +1,33 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class GildedRose {
 
-	private static List<Item> items = null;
+	private List<Item> items = new ArrayList<Item>();
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
+        final GildedRose shop = new GildedRose();
         System.out.println("OMGHAI!");
 		
-        items = new ArrayList<Item>();
-        items.add(new Item("+5 Dexterity Vest", 10, 20));
-        items.add(new Item("Aged Brie", 2, 0));
-        items.add(new Item("Elixir of the Mongoose", 5, 7));
-        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-        items.add(new Item("Conjured Mana Cake", 3, 6));
+        shop.addItem(new Item("+5 Dexterity Vest", 10, 20));
+        shop.addItem(new Item("Aged Brie", 2, 0));
+        shop.addItem(new Item("Elixir of the Mongoose", 5, 7));
+        shop.addItem(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+        shop.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+        shop.addItem(new Item("Conjured Mana Cake", 3, 6));
 
-        updateQuality();
+        shop.updateQuality();
 }
 
-
-	
-    public static void updateQuality()
+    void updateQuality()
     {
         for (int i = 0; i < items.size(); i++)
         {
-            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) 
+            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
             {
                 if (items.get(i).getQuality() > 0)
                 {
@@ -102,4 +99,11 @@ public class GildedRose {
         }
     }
 
+    boolean addItem(final Item e) {
+        return items.add(e);
+    }
+
+    public Item getItem(final int index) {
+        return items.get(index);
+    }
 }
--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,12 +1,66 @@
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class GildedRoseTest {
 
-	@Test
-	public void testTheTruth() {
-		assertTrue(true);
+	private GildedRose shop;
+
+	@Before
+	public void setUp() throws Exception {
+		shop = new GildedRose();
 	}
+
+	@Test
+	public void testThatAnItemCanBeAddedAndRetrieved() {
+		final Item sulfuras = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+		shop.addItem(sulfuras);
+		final Item item = shop.getItem(0);
+		assertThat(item, is(sulfuras));
+	}
+
+	@Test
+	public void testThatSulfurasDoesNotChangeAfterOneDay() {
+		assertQualityAfterOneDay(new Item("Sulfuras, Hand of Ragnaros", 0, 80), 80);
+	}
+
+	@Test
+	public void testThatBrieIncreasesQuality() throws Exception {
+		assertQualityAfterOneDay(new Item("Aged Brie", 2, 0), 1);
+	}
+
+	@Test
+	public void testThatQualityDecreasesFasterAfterSellDate() throws Exception {
+		assertQualityAfterOneDay(new Item("Elixir of the Mongoose", 0, 4), 2);
+	}
+
+	@Test
+	public void testThatQualityIsNeverNegative() throws Exception {
+		assertQualityAfterOneDay(new Item("Elixir of the Mongoose", 1, 0), 0);
+	}
+
+	@Test
+	public void testThatQualityNeverIsBiggerThan50() throws Exception {
+		assertQualityAfterOneDay(new Item("Aged Brie", 1, 50), 50);
+	}
+
+	@Test
+	public void testBackstagePassesMoreThan10DaysLeft() throws Exception {
+		assertQualityAfterOneDay(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20), 21);
+	}
+
+	public void assertQualityAfterOneDay(final Item item, final int expectedQuality) {
+		shop.addItem(item);
+		shop.updateQuality();
+		final Item storedItem = shop.getItem(0);
+		assertThat(storedItem.getQuality(), is(expectedQuality));
+	}
+
+	// TODO: weitere Testfälle für die Backstage Pässe
+	// TODO: Tests für die Zeit
+	// TODO: kosmetisches Refactoring für bequemeres Coden
+	// TODO: eigentliches Refactoring
 }
