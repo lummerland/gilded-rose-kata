cd16882492216d8aa97080f693091cd6b80009c7
Benjamin Peter <bpeter@seibert-media.net>
2016-12-14 14:11:10 +0100

    ! if => if in lower section a - a => 0

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -3,89 +3,87 @@ import java.util.List;
 
 public class GildedRose {
 
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String DEXTERITY_VEST = "+5 Dexterity Vest";
-    public static final String ELIXIR_OF_THE_MONGOOSE = "Elixir of the Mongoose";
-    public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
-    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String CONJURED_MANA_CAKE = "Conjured Mana Cake";
+	public static final String AGED_BRIE = "Aged Brie";
+	public static final String DEXTERITY_VEST = "+5 Dexterity Vest";
+	public static final String ELIXIR_OF_THE_MONGOOSE = "Elixir of the Mongoose";
+	public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+	public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+	public static final String CONJURED_MANA_CAKE = "Conjured Mana Cake";
 
-    private List<Item> items = new ArrayList<Item>();
+	private List<Item> items = new ArrayList<Item>();
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
-        final GildedRose shop = new GildedRose();
-        System.out.println("OMGHAI!");
+		final GildedRose shop = new GildedRose();
+		System.out.println("OMGHAI!");
 
-        shop.addItem(new Item(DEXTERITY_VEST, 10, 20));
-        shop.addItem(new Item(AGED_BRIE, 2, 0));
-        shop.addItem(new Item(ELIXIR_OF_THE_MONGOOSE, 5, 7));
-        shop.addItem(new Item(SULFURAS_HAND_OF_RAGNAROS, 0, 80));
-        shop.addItem(new Item(BACKSTAGE_PASSES, 15, 20));
-        shop.addItem(new Item(CONJURED_MANA_CAKE, 3, 6));
+		shop.addItem(new Item(DEXTERITY_VEST, 10, 20));
+		shop.addItem(new Item(AGED_BRIE, 2, 0));
+		shop.addItem(new Item(ELIXIR_OF_THE_MONGOOSE, 5, 7));
+		shop.addItem(new Item(SULFURAS_HAND_OF_RAGNAROS, 0, 80));
+		shop.addItem(new Item(BACKSTAGE_PASSES, 15, 20));
+		shop.addItem(new Item(CONJURED_MANA_CAKE, 3, 6));
 
-        shop.updateQuality();
-}
+		shop.updateQuality();
+	}
 
-    void updateQuality()
-    {
-        for (Item item : items) {
-            if (SULFURAS_HAND_OF_RAGNAROS.equals(item.getName())) {
-                continue;
-            }
+	void updateQuality() {
+		for (Item item : items) {
+			if (SULFURAS_HAND_OF_RAGNAROS.equals(item.getName())) {
+				continue;
+			}
 
-            if ((AGED_BRIE.equals(item.getName())) || BACKSTAGE_PASSES.equals(item.getName())) {
-                if (item.getQuality() < 50) {
-                    item.setQuality(item.getQuality() + 1);
+			// Besser werdende Produkte
+			if ((AGED_BRIE.equals(item.getName())) || BACKSTAGE_PASSES.equals(item.getName())) {
+				if (item.getQuality() < 50) {
+					item.setQuality(item.getQuality() + 1);
 
-                    if (BACKSTAGE_PASSES.equals(item.getName())) {
-                        if (item.getSellIn() < 11) {
-                            if (item.getQuality() < 50) {
-                                item.setQuality(item.getQuality() + 1);
-                            }
-                        }
+					if (BACKSTAGE_PASSES.equals(item.getName())) {
+						if (item.getSellIn() < 11) {
+							if (item.getQuality() < 50) {
+								item.setQuality(item.getQuality() + 1);
+							}
+						}
 
-                        if (item.getSellIn() < 6) {
-                            if (item.getQuality() < 50) {
-                                item.setQuality(item.getQuality() + 1);
-                            }
-                        }
-                    }
-                }
-            } else {
-                if (item.getQuality() > 0) {
-                    item.setQuality(item.getQuality() - 1);
-                }
-            }
+						if (item.getSellIn() < 6) {
+							if (item.getQuality() < 50) {
+								item.setQuality(item.getQuality() + 1);
+							}
+						}
+					}
+				}
+			} else {
+				if (item.getQuality() > 0) {
+					item.setQuality(item.getQuality() - 1);
+				}
+			}
 
-            item.setSellIn(item.getSellIn() - 1);
+			item.setSellIn(item.getSellIn() - 1);
 
-            if (item.getSellIn() < 0) {
-                if (!AGED_BRIE.equals(item.getName())) {
-                    if (!BACKSTAGE_PASSES.equals(item.getName())) {
-                        if (item.getQuality() > 0) {
-                            item.setQuality(item.getQuality() - 1);
-                        }
-                    } else {
-                        item.setQuality(item.getQuality() - item.getQuality());
-                    }
-                } else {
-                    if (item.getQuality() < 50) {
-                        item.setQuality(item.getQuality() + 1);
-                    }
-                }
-            }
-        }
-    }
+			if (item.getSellIn() < 0) {
+				if (AGED_BRIE.equals(item.getName())) {
+					if (item.getQuality() < 50) {
+						item.setQuality(item.getQuality() + 1);
+					}
+				} else if (BACKSTAGE_PASSES.equals(item.getName())) {
+					item.setQuality(0);
+				} else {
+					if (item.getQuality() > 0) {
+						item.setQuality(item.getQuality() - 1);
+					}
+				}
+			}
+		}
+	}
 
-    boolean addItem(final Item e) {
-        return items.add(e);
-    }
+	boolean addItem(final Item e) {
+		return items.add(e);
+	}
 
-    public Item getItem(final int index) {
-        return items.get(index);
-    }
+	public Item getItem(final int index) {
+		return items.get(index);
+	}
 }
--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -16,7 +16,7 @@ public class GildedRoseTest {
 
 	@Test
 	public void testThatAnItemCanBeAddedAndRetrieved() {
-		final Item sulfuras = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+		final Item sulfuras = new Item(GildedRose.AGED_BRIE, 0, 80);
 		shop.addItem(sulfuras);
 		final Item item = shop.getItem(0);
 		assertThat(item, is(sulfuras));
@@ -24,68 +24,99 @@ public class GildedRoseTest {
 
 	@Test
 	public void testThatSulfurasDoesNotChangeAfterOneDay() {
-		assertQualityAfterOneDay(new Item("Sulfuras, Hand of Ragnaros", 0, 80), 80);
+		assertQualityAfterDays(new Item(GildedRose.SULFURAS_HAND_OF_RAGNAROS, 0, 80), 1, 80);
+	}
+
+	@Test
+	public void testThatSulfurasDoesNotChangeAfterTwoDays() {
+		assertQualityAfterDays(new Item(GildedRose.SULFURAS_HAND_OF_RAGNAROS, 0, 80), 2, 80);
 	}
 
 	@Test
 	public void testThatBrieIncreasesQuality() throws Exception {
-		assertQualityAfterOneDay(new Item("Aged Brie", 2, 0), 1);
+		assertQualityAfterDays(new Item(GildedRose.AGED_BRIE, 2, 0), 1, 1);
 	}
 
 	@Test
 	public void testThatQualityDecreasesFasterAfterSellDate() throws Exception {
-		assertQualityAfterOneDay(new Item("Elixir of the Mongoose", 0, 4), 2);
+		assertQualityAfterDays(new Item(GildedRose.ELIXIR_OF_THE_MONGOOSE, 0, 4), 1, 2);
 	}
 
 	@Test
 	public void testThatQualityIsNeverNegative() throws Exception {
-		assertQualityAfterOneDay(new Item("Elixir of the Mongoose", 1, 0), 0);
+		assertQualityAfterDays(new Item(GildedRose.ELIXIR_OF_THE_MONGOOSE, 1, 0), 1, 0);
 	}
 
 	@Test
 	public void testThatQualityNeverIsBiggerThan50() throws Exception {
-		assertQualityAfterOneDay(new Item("Aged Brie", 1, 50), 50);
+		assertQualityAfterDays(new Item(GildedRose.AGED_BRIE, 1, 50), 1, 50);
 	}
 
 	@Test
 	public void testBackstagePassesMoreThan10DaysLeft() throws Exception {
-		assertQualityAfterOneDay(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20), 21);
+		assertQualityAfterDays(new Item(GildedRose.BACKSTAGE_PASSES, 15, 20), 1, 21);
 	}
 
 	@Test
 	public void testBackstagePasses10DaysLeft() throws Exception {
-		assertQualityAfterOneDay(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20), 22);
+		assertQualityAfterDays(new Item(GildedRose.BACKSTAGE_PASSES, 10, 20), 1, 22);
 	}
 
 	@Test
 	public void testBackstagePasses9DaysLeft() throws Exception {
-		assertQualityAfterOneDay(new Item("Backstage passes to a TAFKAL80ETC concert", 9, 20), 22);
+		assertQualityAfterDays(new Item(GildedRose.BACKSTAGE_PASSES, 9, 20), 1, 22);
 	}
 
 	@Test
 	public void testBackstagePasses5DaysOrLessLeft() throws Exception {
-		assertQualityAfterOneDay(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10), 13);
+		assertQualityAfterDays(new Item(GildedRose.BACKSTAGE_PASSES, 5, 10), 1, 13);
 	}
 
 	@Test
 	public void testBackstagePasses1DaysLeftShouldIncreaseQualityBy3() throws Exception {
-		assertQualityAfterOneDay(new Item("Backstage passes to a TAFKAL80ETC concert", 1, 10), 13);
+		assertQualityAfterDays(new Item(GildedRose.BACKSTAGE_PASSES, 1, 10), 1, 13);
 	}
 
 	@Test
 	public void testBackstagePassesLoseAllTheirValueAfterTheConcert() throws Exception {
-		assertQualityAfterOneDay(new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10), 0);
+		assertQualityAfterDays(new Item(GildedRose.BACKSTAGE_PASSES, 0, 10), 1, 0);
 	}
 
-	public void assertQualityAfterOneDay(final Item item, final int expectedQuality) {
+	@Test
+	public void testThatBrieIncreasesQualityOverMultipleDays() throws Exception {
+		assertQualityAfterDays(new Item(GildedRose.AGED_BRIE, 5, 1), 2, 3);
+	}
+
+	@Test
+	public void testThatSellInDecreasesOverTime() {
+		Item item = new Item(GildedRose.AGED_BRIE, 5, 1);
 		shop.addItem(item);
 		shop.updateQuality();
+		assertThat(shop.getItem(0).getSellIn(), is(4));
+
+		shop.updateQuality();
+		assertThat(shop.getItem(0).getSellIn(), is(3));
+	}
+
+	@Test
+	public void testThatSellInOfSulfurasDoesNotChangeOverTime() {
+		Item item = new Item(GildedRose.SULFURAS_HAND_OF_RAGNAROS, 5, 1);
+		shop.addItem(item);
+		shop.updateQuality();
+		assertThat(shop.getItem(0).getSellIn(), is(5));
+
+		shop.updateQuality();
+		assertThat(shop.getItem(0).getSellIn(), is(5));
+	}
+
+	public void assertQualityAfterDays(final Item item, int days, final int expectedQuality) {
+		shop.addItem(item);
+		for (int i = 0; i < days; i++)
+			shop.updateQuality();
 		final Item storedItem = shop.getItem(0);
 		assertThat(storedItem.getQuality(), is(expectedQuality));
 	}
 
-	// TODO: weitere Testfälle für die Backstage Pässe
-	// TODO: Tests für die Zeit
 	// TODO: kosmetisches Refactoring für bequemeres Coden
 	// TODO: eigentliches Refactoring
 }
